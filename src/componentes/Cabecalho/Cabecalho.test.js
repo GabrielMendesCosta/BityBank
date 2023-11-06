df94a2b933b5742994e904369f61861249a9dea2
GabrielMendesCosta <gabrielmendescst@gmail.com>
2023-11-06 11:41:09 -0300

    test: add the test to Menu

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -1,8 +1,8 @@
-import {render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
 import Cabecalho from '.';
 
 test('Deve renderizar o nome do usuário logado', () => {
-    render(<Cabecalho/>)
-    const nomeUsuario = screen.getByText('Joana Fonseca Gomes')
-    expect(nomeUsuario).toBeInTheDocument();
+  render(<Cabecalho />);
+  const nomeUsuario = screen.getByText('Joana Fonseca Gomes');
+  expect(nomeUsuario).toBeInTheDocument();
 });
